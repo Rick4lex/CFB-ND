eb08cb035b218486e5ab860e45a81e6f51f24357
Ricardo Álvarez M. <140867844+Rick4lex@users.noreply.github.com>
2026-04-10 15:16:31 -0500

    IMG

--- data/landingContent.ts
+++ data/landingContent.ts
@@ -7,16 +7,26 @@ export const carouselImages = [
     { 
         id: 2, 
         url: 'https://res.cloudinary.com/dyeppbrfl/image/upload/q_auto/f_auto/v1775428765/CFB_Post_7_ho3qms.jpg', 
-        alt: 'Gestión de Seguridad Social 2' 
+        alt: 'SMLV' 
+    },
+    { 
+        id: 5, 
+        url: 'https://res.cloudinary.com/dyeppbrfl/image/upload/q_auto/f_auto/v1775851187/CFB_Post_1_jir2rg.jpg', 
+        alt: 'ARL xDIAS' 
     },
     { 
         id: 3, 
         url: 'https://res.cloudinary.com/dyeppbrfl/image/upload/q_auto/f_auto/v1775428477/CFB_WHistory_3_yrj0m0.jpg', 
-        alt: 'Gestión de Seguridad Social 3' 
+        alt: 'ARL EXPRESS' 
     },
     { 
         id: 4, 
         url: 'https://res.cloudinary.com/dyeppbrfl/image/upload/q_auto/f_auto/v1775428412/CFB_Post_13_xzohe9.jpg', 
-        alt: 'Gestión de Seguridad Social 4' 
+        alt: 'Planilla Y' 
+    },
+    { 
+        id: 6, 
+        url: 'https://res.cloudinary.com/dyeppbrfl/image/upload/q_auto/f_auto/v1775851187/CFB_Post_2_au1qzm.jpg', 
+        alt: 'Planilla' 
     }
 ];
